d288f6d919a1635ef68a15fe73c522977c9f7173
Nils Ekman <nisse.ekman@gmail.com>
2013-10-02 22:53:21 +0200

    Moved key maps to its own file

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,6 +63,7 @@ module.exports = function(grunt) {
     onBuildWrite: convert,
     include: [
       'Keynavigator.js',
+      'Keymappings',
       'Cellfactory.js',
       'Celltable.js'
     ],
